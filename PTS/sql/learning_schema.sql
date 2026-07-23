1b7c4bc3d0e32adc534dd22fe5637f535b3fe33c
Ncry46 <nicharat.kpt@gmail.com>
2026-07-23 14:13:20 +0700

    ล่าสุด

---
+++ PTS/sql/learning_schema.sql
@@ -0,0 +1,6 @@
+-- PTS Learning schema extensions
+-- Backend จะสร้างให้อัตโนมัติตอน start แต่สามารถรันด้วยมือได้
+
+-- course_enrollments, course_lessons, lesson_progress,
+-- class_schedules, certificates, payments
+-- ดูรายละเอียดใน backend/ensureSchema.js
